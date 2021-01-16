02525a482b526e63d6edf5cbb37917188abc21bc
Shiluo <wushouyue@gmail.com>
2021-01-16 00:25:32 -0500

    New Helper function that could help reading and writing file(s) from graph/ folder

---
+++ helper/graph_reader.py
@@ -0,0 +1,21 @@
+import json
+import pickle
+
+save_type_JSON = 1
+save_type_pickle = 2
+
+
+def graph_reader(result_file_path, save_type, save_filename_list):
+    result_list = []
+    for save_filename in save_filename_list:
+        if save_type == save_type_pickle:
+            temp_filepath = result_file_path / "{}.p".format(save_filename)
+            with open(temp_filepath, 'rb') as f:
+                result_list.append(pickle.load(f))
+                print("{} loaded".format(temp_filepath))
+        elif save_type == save_type_JSON:
+            temp_filepath = result_file_path / "{}.json".format(save_filename)
+            with open(temp_filepath, 'r') as f:
+                result_list.append(json.load(f))
+                print("{} loaded".format(temp_filepath))
+    return result_list
---
+++ helper/graph_writer.py
@@ -0,0 +1,20 @@
+import json
+import pickle
+
+save_type_JSON = 1
+save_type_pickle = 2
+
+
+def graph_writer(result_file_path, save_type, save_filename_list, save_variable_list):
+    for save_filename, save_variable in zip(save_filename_list, save_variable_list):
+        if save_type == save_type_pickle:
+            temp_filepath = result_file_path / "{}.p".format(save_filename)
+            with open(temp_filepath, 'wb') as f:
+                pickle.dump(save_variable, f)
+                print("{} saved".format(temp_filepath))
+        elif save_type == save_type_JSON:
+            temp_filepath = result_file_path / "{}.json".format(save_filename)
+            with open(temp_filepath, 'w') as f:
+                json.dump(save_variable, f, indent=2)
+                print("{} saved".format(temp_filepath))
+    return 0
